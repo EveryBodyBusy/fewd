85fd36e35a77372a8b6d5e884385201577fbe327
EveryBodyBusy <jchiu2@gmail.com>
2016-01-17 20:53:26 -0800

    classwork

--- Week_05_Intro_Programming/07_intro_programming/solution/color_scheme_switcher/js/index.js
+++ Week_05_Intro_Programming/07_intro_programming/solution/color_scheme_switcher/js/index.js
@@ -1,19 +1,30 @@
+$('#grayButton').click(switchGray);
 
-document.getElementById('grayButton').onclick = switchGray;
+// document.getElementById('grayButton').onclick = switchGray;
 document.getElementById('whiteButton').onclick = switchWhite;
 document.getElementById('blueButton').onclick = switchBlue;
+document.getElementById('pizza').onclick = pizzatime;
+
 
 function switchGray() {
   document.body.style.backgroundColor = '#66ff66';
- document.body.style.color = 'white';
+document.body.style.color = 'peachpuff';
+document.getElementById('pizza').style.color = 'peachpuff';
 }
 
 function switchWhite() {
   document.body.style.backgroundColor = 'white';
-  document.body.style.color = 'black';
+  document.body.style.color = 'pink';
+  document.getElementById('pizza').style.color = 'pink';
+
 }
 
 function switchBlue() {
   document.body.style.backgroundColor = 'blue';
   document.body.style.color = 'white';
+  document.getElementById('pizza').style.color = 'white';
+}
+
+function pizzatime() {
+document.getElementById('pizza').style.color = 'red';
 }
